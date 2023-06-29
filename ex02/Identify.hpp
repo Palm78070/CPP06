5b45a36fcc2e4861b2178a2b289e58e2b90c6c29
Rath Thammatucharee <rthammat@z3t4c3.42bangkok.com>
2023-06-29 17:37:56 +0700

    ex02 add header for rand() srand()

--- ex02/Identify.hpp
+++ ex02/Identify.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Identify.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rthammat <rthammat@42.fr>                  +#+  +:+       +#+        */
+/*   By: rthammat <rthammat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 00:47:05 by rthammat          #+#    #+#             */
-/*   Updated: 2023/06/26 01:32:00 by rthammat         ###   ########.fr       */
+/*   Updated: 2023/06/29 17:37:18 by rthammat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include <cstdlib>
+#include <ctime>
 
 Base *generate(void);
 void identify(Base *p);
